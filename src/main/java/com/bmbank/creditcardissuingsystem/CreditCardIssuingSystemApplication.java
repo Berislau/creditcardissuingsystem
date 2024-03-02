8fc4e8faa8a1f43ad4e0beee1fb31156a2729fb7
Berislav Mikulić <github.bm@gmail.com>
2024-03-02 12:24:49 +0100

    Initial commit

---
+++ src/main/java/com/bmbank/creditcardissuingsystem/CreditCardIssuingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bmbank.creditcardissuingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardIssuingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardIssuingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmbank/creditcardissuingsystem/CreditCardIssuingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmbank.creditcardissuingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardIssuingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
